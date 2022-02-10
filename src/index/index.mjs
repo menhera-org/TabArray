30819ecbcfb4937e8c4bd4e9f87a7f88d649283d
metastable-void <git@yuka.h09.eu>
2022-02-10 20:22:29 +0900

    show container icon for index tabs

--- src/index/index.mjs
+++ src/index/index.mjs
@@ -22,11 +22,22 @@ import { IndexTab } from "../modules/IndexTab.mjs";
 const indexTab = new IndexTab(location.href);
 document.title = indexTab.title;
 
-const svgSource = `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 256'>
-<rect x='0' y='0' width='256' height='256' fill='${indexTab.colorCode}'/>
-</svg>`;
-const blob = new Blob([svgSource], {
-  type: 'image/svg+xml',
+const rect = document.querySelector('#rect');
+
+rect.setAttribute('fill', indexTab.colorCode);
+rect.style.mask = `url(${indexTab.iconUrl}) center / contain no-repeat`;
+
+browser.tabs.getCurrent().then(async (tabObj) => {
+  const tabId = tabObj.id;
+  const imageUrl = await browser.tabs.captureTab(tabId, {
+    scale: 1,
+    rect: {
+      x: 0,
+      y: 0,
+      width: 256,
+      height: 256,
+    }
+  });
+  document.querySelector(`link[rel="icon"]`).href = imageUrl;
 });
-const objectUrl = URL.createObjectURL(blob)
-document.querySelector(`link[rel="icon"]`).href = objectUrl;
+
