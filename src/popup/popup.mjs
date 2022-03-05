98319c44997132df7dd6126b0bc34393159aa03a
metastable-void <git@yuka.h09.eu>
2022-03-05 20:26:37 +0900

    sidebar

--- src/popup/popup.mjs
+++ src/popup/popup.mjs
@@ -30,6 +30,86 @@ import { IndexTab } from '../modules/IndexTab.mjs';
 
 import {config} from '../modules/config.mjs';
 
+document.body.innerHTML = `
+<div id='main' class='content'>
+	<div id='topbox'>
+		<div id='search-wrapper'>
+			<input type='search' id='search'/>
+		</div>
+		<button id='button-new-container'>
+			<span class='button-text'>buttonNewContainer</span>
+		</button>
+		<button id='button-sidebar'>
+			<span class='button-text'>buttonSidebar</span>
+		</button>
+		<button id='button-panorama'>
+			<span class='button-text'>buttonPanorama</span>
+		</button>
+	</div>
+	<div id='main-inner' class='menu-part'>
+		<ul id='menuList' class='menu-list'></ul>
+	</div>
+</div>
+<div id='windows' class='content'>
+	<div id='windows-inner' class='menu-part'>
+		<button id='button-new-window'>
+			<span class='button-text'>buttonNewWindow</span>
+		</button>
+		<ul id='windowMenuList' class='menu-list'></ul>
+	</div>
+</div>
+<div id='sites' class='content' data-active-content='sites'>
+	<div id='sites-pane-top' data-content-id='sites'></div>
+	<div id='sites-pane-details' data-content-id='sites-details'>
+		<div class='heading'>
+			<button id='site-pane-details-back-button'></button>
+			<div id='site-pane-details-domain'></div>
+		</div>
+		<div id='siteMenuList' class='menu-list'></div>
+	</div>
+</div>
+<div id='bottombox' class='menu-part'>
+	<nav id='bottom-menu'>
+		<a id='menu-item-main' class='menu-item' href='#main'>
+			<span class='button-text'>menuItemContainers</span>
+		</a>
+		<a id='menu-item-windows' class='menu-item' href='#windows'>
+			<span class='button-text'>menuItemWindows</span>
+		</a>
+		<a id='menu-item-sites' class='menu-item' href='#sites'>
+			<span class='button-text'>menuItemSites</span>
+		</a>
+		<a id='menu-item-settings' class='menu-item' href='#settings'>
+			<span class='button-text'>buttonSettings</span>
+		</a>
+	</nav>
+</div>
+<div id='confirm' class='modal'>
+	<div class='modal-content'>
+		<p id='confirm-message'></p>
+		<div class='modal-actions'>
+			<button id='confirm-cancel-button'>buttonCancel</button>
+			<button id='confirm-ok-button' class='button-default'>buttonOk</button>
+		</div>
+	</div>
+</div>
+<div id='new-container' class='modal'>
+	<div class='modal-content'>
+		<h2 class='modal-title'></h2>
+		<div class='input-box'>
+			<label for='new-container-name'>newContainerNameLabel</label>
+			<input class='input-box-input' type='text' name='name' id='new-container-name' placeholder='newContainerNamePlaceholder'/>
+		</div>
+		<usercontext-iconpicker id='new-container-icon'></usercontext-iconpicker>
+		<usercontext-colorpicker id='new-container-color'></usercontext-colorpicker>
+		<div class='modal-actions'>
+			<button id='new-container-cancel-button'>buttonCancel</button>
+			<button id='new-container-ok-button' class='button-default'>buttonOk</button>
+		</div>
+	</div>
+</div>
+`;
+
 const STATE_NO_TABS = 0;
 const STATE_HIDDEN_TABS = 1;
 const STATE_VISIBLE_TABS = 2;
@@ -40,6 +120,8 @@ document.documentElement.lang = browser.i18n.getMessage('effectiveLocale');
 document.title = browser.i18n.getMessage('browserActionPopupTitle');
 document.querySelector('#button-panorama > .button-text').textContent = browser.i18n.getMessage('buttonPanorama');
 document.querySelector('#button-panorama').title = browser.i18n.getMessage('buttonPanorama');
+document.querySelector('#button-sidebar > .button-text').textContent = browser.i18n.getMessage('buttonSidebar');
+document.querySelector('#button-sidebar').title = browser.i18n.getMessage('buttonSidebar');
 document.querySelector('#button-new-container > .button-text').textContent = browser.i18n.getMessage('buttonNewContainer');
 document.querySelector('#button-new-container').title = browser.i18n.getMessage('buttonNewContainer');
 document.querySelector('#confirm-cancel-button').textContent = browser.i18n.getMessage('buttonCancel');
@@ -53,6 +135,12 @@ document.querySelector('#menu-item-windows > .button-text').textContent = browse
 document.querySelector('#menu-item-sites > .button-text').textContent = browser.i18n.getMessage('menuItemSites');
 document.querySelector('#menu-item-settings > .button-text').textContent = browser.i18n.getMessage('buttonSettings');
 document.querySelector('#button-new-window > .button-text').textContent = browser.i18n.getMessage('buttonNewWindow');
+
+document.querySelector('#button-sidebar').addEventListener('click', (ev) => {
+	browser.sidebarAction.open().catch((e) => {
+		console.error(e);
+	});
+});
 
 document.querySelector('#main').classList.add('rendering');
 
