3844bb3de086c72082956f5232b7b13b2c023d8b
metastable-void <git@yuka.h09.eu>
2022-09-05 19:51:06 +0900

    make npm run clean run in WIndows

---
+++ clean.js
@@ -0,0 +1,51 @@
+// -*- indent-tabs-mode: nil; tab-width: 2; -*-
+// vim: set ts=&2 sw=2 et ai :
+
+/*
+  Container Tab Groups
+  Copyright (C) 2022 Menhera.org
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+  Run with `make clean` and `npm run clean`
+*/
+
+const path = require('path');
+const fs = require('fs');
+
+const findAndClear = function find(startPath, filter) {
+  if (!fs.existsSync(startPath)) {
+    return;
+  }
+
+  var files = fs.readdirSync(startPath);
+  for (let i = 0; i < files.length; i++) {
+    const filename = path.join(startPath, files[i]);
+    const stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      // recursive search
+      find(filename, filter);
+    } else if (filename.endsWith(filter)) {
+      console.log('removing:', filename);
+      fs.unlinkSync(filename);
+    };
+  };
+};
+
+const distDir = path.join(__dirname, 'dist');
+const buildsDir = path.join(__dirname, 'builds');
+findAndClear(distDir, '.js');
+fs.rmSync(buildsDir, { recursive: true, force: true });
