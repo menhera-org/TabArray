3dbab0ae07db1799f266fab6f2e52843741d2f05
metastable-void <git@yuka.h09.eu>
2022-10-20 18:39:03 +0900

    script/clean-builds.js

---
+++ script/clean-builds.js
@@ -0,0 +1,34 @@
+// -*- indent-tabs-mode: nil; tab-width: 2; -*-
+// vim: set ts=2 sw=2 et ai :
+
+/*
+  Container Tab Groups
+  Copyright (C) 2022 Menhera.org
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/* eslint-env es2020, node */
+
+/*
+  Run with `make clean` and `npm run clean`
+*/
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+
+const buildsDir = path.join(__dirname, '../builds');
+fs.rmSync(buildsDir, { recursive: true, force: true });
