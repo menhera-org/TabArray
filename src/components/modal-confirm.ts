54567ba01f66762cb13eef345310db12d33a5af8
metastable-void <git@yuka.h09.eu>
2023-02-16 17:03:17 +0900

    modal-confirm element

---
+++ src/components/modal-confirm.ts
@@ -0,0 +1,76 @@
+// -*- indent-tabs-mode: nil; tab-width: 2; -*-
+// vim: set ts=2 sw=2 et ai :
+
+/*
+  Container Tab Groups
+  Copyright (C) 2023 Menhera.org
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import browser from 'webextension-polyfill';
+import { EventSink } from '../frameworks/utils';
+
+export class ModalConfirmElement extends HTMLElement {
+  public readonly onContainerCreated = new EventSink<string>();
+  public readonly onContainerUpdated = new EventSink<string>();
+  public readonly onCancel = new EventSink<void>();
+  public readonly onOk = new EventSink<void>();
+
+  public constructor(title: string) {
+    super();
+    this.attachShadow({ mode: 'open' });
+    if (!this.shadowRoot) {
+      throw new Error('Shadow root is null');
+    }
+
+    const styleSheet = document.createElement('link');
+    styleSheet.rel = 'stylesheet';
+    styleSheet.href = '/components/modal-confirm.css';
+    this.shadowRoot.appendChild(styleSheet);
+
+    const modalContent = document.createElement('div');
+    modalContent.id = 'modal-content';
+    const modalTitle = document.createElement('h2');
+    modalTitle.id = 'modal-title';
+    modalContent.appendChild(modalTitle);
+    modalTitle.textContent = title;
+
+    const modalActions = document.createElement('div');
+    modalActions.id = 'modal-actions';
+    const cancelButton = document.createElement('button');
+    cancelButton.id = 'cancel-button';
+    cancelButton.textContent = browser.i18n.getMessage('buttonCancel');
+    modalActions.appendChild(cancelButton);
+    const okButton = document.createElement('button');
+    okButton.id = 'ok-button';
+    okButton.textContent = browser.i18n.getMessage('buttonOk');
+    modalActions.appendChild(okButton);
+    modalContent.appendChild(modalActions);
+
+    this.shadowRoot.appendChild(modalContent);
+
+    cancelButton.addEventListener('click', () => {
+      this.onCancel.dispatch();
+      this.remove();
+    });
+
+    okButton.addEventListener('click', () => {
+      this.onOk.dispatch();
+      this.remove();
+    });
+  }
+}
+
+customElements.define('modal-confirm', ModalConfirmElement);
