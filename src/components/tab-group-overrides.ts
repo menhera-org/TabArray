4b3934b24819823161e5ae7d0ab27e92379313fc
metastable-void <git@yuka.h09.eu>
2023-04-21 19:29:04 +0900

    extracted common codes from tab-group-overrides and tab-group-sorter

--- src/components/tab-group-overrides.ts
+++ src/components/tab-group-overrides.ts
@@ -20,74 +20,22 @@
 **/
 
 import browser from 'webextension-polyfill';
-import { CookieStore, DisplayedContainer } from 'weeg-containers';
 
-import { SupergroupType, TabGroupDirectory } from '../lib/tabGroups/TabGroupDirectory';
-import { ContextualIdentityService } from '../lib/tabGroups/ContextualIdentityService';
-import { TabGroupAttributes } from '../lib/tabGroups/TabGroupAttributes';
-import { TabGroupDirectorySnapshot } from '../lib/tabGroups/TabGroupDirectorySnapshot';
-import { TabGroupService } from '../lib/tabGroups/TabGroupService';
 import { LanguageSettings } from '../lib/overrides/LanguageSettings';
 import { UserAgentSettings, UserAgentPreset } from '../lib/overrides/UserAgentSettings';
+
 import { config } from '../config/config';
 
-export class TabGroupOverridesElement extends HTMLElement {
-  private readonly _tabGroupService = TabGroupService.getInstance();
-  private readonly _tabGroupDirectory = this._tabGroupService.directory;
-  private readonly _contextualIdentityService = ContextualIdentityService.getInstance();
-  private readonly _contextualIdentityFactory = this._contextualIdentityService.getFactory();
-  private readonly _displayedContainerFactory = this._contextualIdentityService.getDisplayedContainerFactory();
+import { TabGroupListingElement } from './tab-group-listing';
+
+export class TabGroupOverridesElement extends TabGroupListingElement {
   private readonly _languageSettings = LanguageSettings.getInstance();
   private readonly _userAgentSettings = UserAgentSettings.getInstance();
 
   public constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    if (!this.shadowRoot) {
-      throw new Error('Shadow root is null');
-    }
-
-    const styleSheet = document.createElement('link');
-    styleSheet.rel = 'stylesheet';
-    styleSheet.href = '/css/components/tab-group-overrides.css';
-    this.shadowRoot.appendChild(styleSheet);
-
-    const sorterWrapperElement = document.createElement('div');
-    sorterWrapperElement.id = 'sorter-wrapper';
-    this.shadowRoot.appendChild(sorterWrapperElement);
-
-    const headerElement = document.createElement('div');
-    headerElement.classList.add('header');
-    sorterWrapperElement.appendChild(headerElement);
-
-    const headerContainerElement = document.createElement('div');
-    headerContainerElement.classList.add('header-container');
-    headerContainerElement.textContent = browser.i18n.getMessage('menuItemMain');
-    headerElement.appendChild(headerContainerElement);
-
-    const headerLanguagesElement = document.createElement('div');
-    headerLanguagesElement.classList.add('header-languages');
-    headerLanguagesElement.textContent = browser.i18n.getMessage('optionsLabelLanguages');
-    headerElement.appendChild(headerLanguagesElement);
-
-    const headerUserAgentElement = document.createElement('div');
-    headerUserAgentElement.classList.add('header-user-agent');
-    headerUserAgentElement.textContent = browser.i18n.getMessage('optionsLabelUserAgent');
-    headerElement.appendChild(headerUserAgentElement);
-
-    const groupsElement = document.createElement('div');
-    groupsElement.id = 'groups';
-    sorterWrapperElement.appendChild(groupsElement);
-
-    this.render().catch((e) => {
-      console.error(e);
-    });
-
-    this._tabGroupDirectory.onChanged.addListener(() => {
-      this.render().catch((e) => {
-        console.error(e);
-      });
-    });
+    const languageColumnName = browser.i18n.getMessage('optionsLabelLanguages');
+    const userAgentColumnName = browser.i18n.getMessage('optionsLabelUserAgent');
+    super(false, '/css/components/tab-group-overrides.css', [languageColumnName, userAgentColumnName]);
   }
 
   private createLanguageOptionsElement(tabGroupId: string): HTMLInputElement {
@@ -191,109 +139,15 @@ export class TabGroupOverridesElement extends HTMLElement {
     return element;
   }
 
-  private renderCookieStore(nestingCount: number, displayedContainer: DisplayedContainer): HTMLDivElement {
-    const element = document.createElement('div');
-    element.classList.add('container');
-
-    const containerInnerElement = document.createElement('div');
-    containerInnerElement.classList.add('container-inner');
-    element.appendChild(containerInnerElement);
-
-    containerInnerElement.style.marginInlineStart = `${nestingCount * 2}em`;
-
-    const iconElement = document.createElement('div');
-    iconElement.classList.add('icon');
-    if (displayedContainer.cookieStore.userContextId == 0) {
-      iconElement.style.mask = `url(${displayedContainer.iconUrl}) center center / 75% no-repeat`;
-      iconElement.classList.add('masked');
-      iconElement.style.backgroundColor = displayedContainer.colorCode;
-    } else {
-      iconElement.style.background = `url(${displayedContainer.iconUrl}) center center / 75% no-repeat`;
-      containerInnerElement.appendChild(iconElement);
+  protected override createColumnElement(columnId: number, tabGroupId: string): HTMLElement {
+    switch (columnId) {
+      case 0:
+        return this.createLanguageOptionsElement(tabGroupId);
+      case 1:
+        return this.createUserAgentOptionsElement(tabGroupId);
+      default:
+        throw new Error(`Unexpected column ID: ${columnId}`);
     }
-
-    const nameElement = document.createElement('div');
-    nameElement.classList.add('name');
-    nameElement.textContent = displayedContainer.name;
-    containerInnerElement.appendChild(nameElement);
-
-    const languageOptions = this.createLanguageOptionsElement(displayedContainer.cookieStore.id);
-    element.appendChild(languageOptions);
-
-    const userAgentOptions = this.createUserAgentOptionsElement(displayedContainer.cookieStore.id);
-    element.appendChild(userAgentOptions);
-
-    return element;
-  }
-
-  private renderSupergroupElement(nestingCount: number, supergroup: SupergroupType): HTMLDivElement {
-    const element = document.createElement('div');
-    element.classList.add('container');
-
-    const containerInnerElement = document.createElement('div');
-    containerInnerElement.classList.add('container-inner');
-    element.appendChild(containerInnerElement);
-
-    containerInnerElement.style.marginInlineStart = `${nestingCount * 2}em`;
-
-    const iconElement = document.createElement('div');
-    iconElement.classList.add('icon');
-    iconElement.style.mask = `url(/img/material-icons/folder.svg) center center / 75% no-repeat`;
-    iconElement.classList.add('masked');
-    containerInnerElement.appendChild(iconElement);
-
-    const nameElement = document.createElement('div');
-    nameElement.classList.add('name');
-    nameElement.textContent = supergroup.name;
-    containerInnerElement.appendChild(nameElement);
-
-    const tabGroupId = TabGroupAttributes.getTabGroupIdFromSupergroupId(supergroup.supergroupId);
-
-    const languageOptions = this.createLanguageOptionsElement(tabGroupId);
-    element.appendChild(languageOptions);
-
-    const userAgentOptions = this.createUserAgentOptionsElement(tabGroupId);
-    element.appendChild(userAgentOptions);
-
-    return element;
-  }
-
-  private renderSupergroup(nestingCount: number, supergroup: SupergroupType, displayedContainerMap: Map<string, DisplayedContainer>, tabGroupDirectorySnapshot: TabGroupDirectorySnapshot, element: HTMLElement): void {
-    for (const tabGroupId of supergroup.members) {
-      const attributes = new TabGroupAttributes(tabGroupId);
-      if (attributes.tabGroupType == 'cookieStore') {
-        const contextualIdentity = displayedContainerMap.get(tabGroupId);
-        if (!contextualIdentity) continue;
-        const containerElement = this.renderCookieStore(nestingCount, contextualIdentity);
-        element.appendChild(containerElement);
-      } else {
-        const supergroup = tabGroupDirectorySnapshot.getSupergroup(tabGroupId) as SupergroupType;
-        const supergroupElement = this.renderSupergroupElement(nestingCount, supergroup);
-        element.appendChild(supergroupElement);
-        this.renderSupergroup(nestingCount + 1, supergroup, displayedContainerMap, tabGroupDirectorySnapshot, element);
-      }
-    }
-  }
-
-  public async render(): Promise<void> {
-    const groupsElement = this.shadowRoot?.getElementById('groups') as HTMLDivElement;
-    const snapshot = await this._tabGroupDirectory.getSnapshot();
-    const cookieStoreIds = snapshot.getContainerOrder();
-    const displayedContainerMap = new Map<string, DisplayedContainer>();
-    for (const cookieStoreId of cookieStoreIds) {
-      const cookieStore = new CookieStore(cookieStoreId);
-      if (cookieStore.userContextId == 0) {
-        const displayedContainer = this._displayedContainerFactory.createFromCookieStore(cookieStore);
-        displayedContainerMap.set(cookieStoreId, displayedContainer);
-      } else {
-        const contextualIdentity = await this._contextualIdentityFactory.get(cookieStoreId);
-        displayedContainerMap.set(cookieStoreId, contextualIdentity);
-      }
-    }
-    const rootSupergroupTabGroupId = TabGroupDirectory.getRootSupergroupId();
-    const rootSupergroup = snapshot.getSupergroup(rootSupergroupTabGroupId) as SupergroupType;
-    groupsElement.textContent = '';
-    this.renderSupergroup(0, rootSupergroup, displayedContainerMap, snapshot, groupsElement);
   }
 }
 
--- src/components/tab-group-sorter.ts
+++ src/components/tab-group-sorter.ts
@@ -20,65 +20,15 @@
 **/
 
 import browser from 'webextension-polyfill';
-import { CookieStore, DisplayedContainer } from 'weeg-containers';
 
-import { SupergroupType, TabGroupDirectory } from '../lib/tabGroups/TabGroupDirectory';
-import { ContextualIdentityService } from '../lib/tabGroups/ContextualIdentityService';
-import { TabGroupAttributes } from '../lib/tabGroups/TabGroupAttributes';
-import { TabGroupDirectorySnapshot } from '../lib/tabGroups/TabGroupDirectorySnapshot';
-import { TabGroupService } from '../lib/tabGroups/TabGroupService';
+import { TabGroupListingElement } from './tab-group-listing';
 
-export class TabGroupSorterElement extends HTMLElement {
-  private readonly _tabGroupService = TabGroupService.getInstance();
-  private readonly _tabGroupDirectory = this._tabGroupService.directory;
-  private readonly _tabGroupOptionDirectory = this._tabGroupService.optionDirectory;
-  private readonly _contextualIdentityService = ContextualIdentityService.getInstance();
-  private readonly _contextualIdentityFactory = this._contextualIdentityService.getFactory();
-  private readonly _displayedContainerFactory = this._contextualIdentityService.getDisplayedContainerFactory();
+export class TabGroupSorterElement extends TabGroupListingElement {
+  private readonly _tabGroupOptionDirectory = this.tabGroupService.optionDirectory;
 
   public constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    if (!this.shadowRoot) {
-      throw new Error('Shadow root is null');
-    }
-
-    const styleSheet = document.createElement('link');
-    styleSheet.rel = 'stylesheet';
-    styleSheet.href = '/css/components/tab-group-sorter.css';
-    this.shadowRoot.appendChild(styleSheet);
-
-    const sorterWrapperElement = document.createElement('div');
-    sorterWrapperElement.id = 'sorter-wrapper';
-    this.shadowRoot.appendChild(sorterWrapperElement);
-
-    const headerElement = document.createElement('div');
-    headerElement.classList.add('header');
-    sorterWrapperElement.appendChild(headerElement);
-
-    const headerContainerElement = document.createElement('div');
-    headerContainerElement.classList.add('header-container');
-    headerContainerElement.textContent = browser.i18n.getMessage('menuItemMain');
-    headerElement.appendChild(headerContainerElement);
-
-    const headerAutocleanElement = document.createElement('div');
-    headerAutocleanElement.classList.add('header-autoclean');
-    headerAutocleanElement.textContent = browser.i18n.getMessage('enableCookiesAutoclean');
-    headerElement.appendChild(headerAutocleanElement);
-
-    const groupsElement = document.createElement('div');
-    groupsElement.id = 'groups';
-    sorterWrapperElement.appendChild(groupsElement);
-
-    this.render().catch((e) => {
-      console.error(e);
-    });
-
-    this._tabGroupDirectory.onChanged.addListener(() => {
-      this.render().catch((e) => {
-        console.error(e);
-      });
-    });
+    const autoCleanColumnName = browser.i18n.getMessage('enableCookiesAutoclean');
+    super(true, '/css/components/tab-group-sorter.css', [autoCleanColumnName]);
   }
 
   private createOptionsElement(cookieStoreId: string): HTMLDivElement {
@@ -104,139 +54,11 @@ export class TabGroupSorterElement extends HTMLElement {
     return options;
   }
 
-  private renderUserContext(nestingCount: number, displayedContainer: DisplayedContainer): HTMLDivElement {
-    const element = document.createElement('div');
-    element.classList.add('container');
-
-    const containerInnerElement = document.createElement('div');
-    containerInnerElement.classList.add('container-inner');
-    element.appendChild(containerInnerElement);
-
-    containerInnerElement.style.marginInlineStart = `${nestingCount * 2}em`;
-
-    const iconElement = document.createElement('div');
-    iconElement.classList.add('icon');
-    if (displayedContainer.cookieStore.userContextId == 0) {
-      iconElement.style.mask = `url(${displayedContainer.iconUrl}) center center / 75% no-repeat`;
-      iconElement.classList.add('masked');
-      iconElement.style.backgroundColor = displayedContainer.colorCode;
-    } else {
-      iconElement.style.background = `url(${displayedContainer.iconUrl}) center center / 75% no-repeat`;
-      containerInnerElement.appendChild(iconElement);
+  protected override createColumnElement(columnId: number, tabGroupId: string): HTMLElement {
+    if (columnId == 0) {
+      return this.createOptionsElement(tabGroupId);
     }
-
-    const nameElement = document.createElement('div');
-    nameElement.classList.add('name');
-    nameElement.textContent = displayedContainer.name;
-    containerInnerElement.appendChild(nameElement);
-
-    const upButton = document.createElement('button');
-    upButton.classList.add('up');
-    containerInnerElement.appendChild(upButton);
-    upButton.addEventListener('click', () => {
-      this._tabGroupDirectory.moveTabGroupUp(displayedContainer.cookieStore.id).catch((e) => {
-        console.error(e);
-      });
-    });
-
-    const downButton = document.createElement('button');
-    downButton.classList.add('down');
-    containerInnerElement.appendChild(downButton);
-    downButton.addEventListener('click', () => {
-      this._tabGroupDirectory.moveTabGroupDown(displayedContainer.cookieStore.id).catch((e) => {
-        console.error(e);
-      });
-    });
-
-    const options = this.createOptionsElement(displayedContainer.cookieStore.id);
-    element.appendChild(options);
-
-    return element;
-  }
-
-  private renderSupergroupElement(nestingCount: number, supergroup: SupergroupType): HTMLDivElement {
-    const element = document.createElement('div');
-    element.classList.add('container');
-
-    const containerInnerElement = document.createElement('div');
-    containerInnerElement.classList.add('container-inner');
-    element.appendChild(containerInnerElement);
-
-    containerInnerElement.style.marginInlineStart = `${nestingCount * 2}em`;
-
-    const iconElement = document.createElement('div');
-    iconElement.classList.add('icon');
-    iconElement.style.mask = `url(/img/material-icons/folder.svg) center center / 75% no-repeat`;
-    iconElement.classList.add('masked');
-    containerInnerElement.appendChild(iconElement);
-
-    const nameElement = document.createElement('div');
-    nameElement.classList.add('name');
-    nameElement.textContent = supergroup.name;
-    containerInnerElement.appendChild(nameElement);
-
-    const tabGroupId = TabGroupAttributes.getTabGroupIdFromSupergroupId(supergroup.supergroupId);
-
-    const upButton = document.createElement('button');
-    upButton.classList.add('up');
-    containerInnerElement.appendChild(upButton);
-    upButton.addEventListener('click', () => {
-      this._tabGroupDirectory.moveTabGroupUp(tabGroupId).catch((e) => {
-        console.error(e);
-      });
-    });
-
-    const downButton = document.createElement('button');
-    downButton.classList.add('down');
-    containerInnerElement.appendChild(downButton);
-    downButton.addEventListener('click', () => {
-      this._tabGroupDirectory.moveTabGroupDown(tabGroupId).catch((e) => {
-        console.error(e);
-      });
-    });
-
-    const options = this.createOptionsElement(tabGroupId);
-    element.appendChild(options);
-
-    return element;
-  }
-
-  private renderSupergroup(nestingCount: number, supergroup: SupergroupType, displayedContainerMap: Map<string, DisplayedContainer>, tabGroupDirectorySnapshot: TabGroupDirectorySnapshot, element: HTMLElement): void {
-    for (const tabGroupId of supergroup.members) {
-      const attributes = new TabGroupAttributes(tabGroupId);
-      if (attributes.tabGroupType == 'cookieStore') {
-        const contextualIdentity = displayedContainerMap.get(tabGroupId);
-        if (!contextualIdentity) continue;
-        const containerElement = this.renderUserContext(nestingCount, contextualIdentity);
-        element.appendChild(containerElement);
-      } else {
-        const supergroup = tabGroupDirectorySnapshot.getSupergroup(tabGroupId) as SupergroupType;
-        const supergroupElement = this.renderSupergroupElement(nestingCount, supergroup);
-        element.appendChild(supergroupElement);
-        this.renderSupergroup(nestingCount + 1, supergroup, displayedContainerMap, tabGroupDirectorySnapshot, element);
-      }
-    }
-  }
-
-  public async render(): Promise<void> {
-    const groupsElement = this.shadowRoot?.getElementById('groups') as HTMLDivElement;
-    const snapshot = await this._tabGroupDirectory.getSnapshot();
-    const cookieStoreIds = snapshot.getContainerOrder();
-    const displayedContainerMap = new Map<string, DisplayedContainer>();
-    for (const cookieStoreId of cookieStoreIds) {
-      const cookieStore = new CookieStore(cookieStoreId);
-      if (cookieStore.userContextId == 0) {
-        const displayedContainer = this._displayedContainerFactory.createFromCookieStore(cookieStore);
-        displayedContainerMap.set(cookieStoreId, displayedContainer);
-      } else {
-        const contextualIdentity = await this._contextualIdentityFactory.get(cookieStoreId);
-        displayedContainerMap.set(cookieStoreId, contextualIdentity);
-      }
-    }
-    const rootSupergroupTabGroupId = TabGroupDirectory.getRootSupergroupId();
-    const rootSupergroup = snapshot.getSupergroup(rootSupergroupTabGroupId) as SupergroupType;
-    groupsElement.textContent = '';
-    this.renderSupergroup(0, rootSupergroup, displayedContainerMap, snapshot, groupsElement);
+    throw new Error('Invalid column id');
   }
 }
 
